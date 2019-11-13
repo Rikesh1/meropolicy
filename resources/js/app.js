6fff0c533992240f098bc605e626670d899632ad
manishadhikari241 <manishadhikari241@gmail.com>
2019-11-13 11:27:58 +0545

    ok

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,4 @@
+require('../../node_modules/jquery/dist/jquery');
 require('./bootstrap');
+require('../../node_modules/AdminLTE-2.4.18/dist/js/adminlte');
+require('../../node_modules/AdminLTE-2.4.18/dist/js/pages/dashboard');
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'Backend', 'prefix' => 'admin'], function () {
+    Route::get('/', 'DashboardController@index')->name('dashboard');
 });
