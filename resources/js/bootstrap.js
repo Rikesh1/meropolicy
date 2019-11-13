eafacacd07821851f7d32f818b44c6b67b6c3691
Rikesh Maharjan <rmaharjan74@gmail.com>
2019-11-13 11:21:38 +0545

    added admin template

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,9 @@
 window._ = require('lodash');
 
+window.$ = window.jQuery = require('jquery');
+require('bootstrap');
+require('admin-lte');
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
---
+++ resources/views/front/layouts/app.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>@yield('title')</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  @include('front.layouts.header')
+  @include('front.layouts.sidebar')
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    @yield('content')
+  </div>
+  @include('front.layouts.footer')
+
+    <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <div class="control-sidebar-bg"></div>
+</div>
+<!-- ./wrapper -->
+
+<!-- AdminLTE App -->
+<script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
