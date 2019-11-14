ec6551608eeb1ed9e4b59d3d9bdec1a2fdd23025
Rikesh Maharjan <rmaharjan74@gmail.com>
2019-11-14 12:22:55 +0545

    authentication started

--- database/migrations/2019_11_13_115227_create_admins_table.php
+++ database/migrations/2019_11_13_115227_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,23 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone')->nullable();
             $table->string('password');
-            $table->text('address')->nullable();
-            $table->string('dob')->nullable();
-            $table->enum('gender', ['Male', 'Female'])->nullable();
-            $table->string('profile_image')->nullable();
-            $table->enum('status', ['Active', 'Inactive'])->default('Active');
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -40,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::group(['namespace' => 'Backend', 'prefix' => 'admin'], function () {
     Route::get('/', 'DashboardController@index')->name('dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+/** Admin Login/Registr */
+Route::get('/login/admin', 'Auth/LoginController@showAdminForm');
+Route::post('/login/admin', 'Auth/LoginController@postAdminForm');
+Route::get('/register/admin', 'Auth/RegisterController@showAdminForm');
+Route::post('/register/admin', 'Auth/RegisterController@postAdminForm');
+
+/** Merchant Login/Register */
+Route::get('/login/merchant', 'Auth/LoginController@showMerchantForm');
+Route::post('/login/merchant', 'Auth/LoginController@postMerchantForm');
+Route::get('/register/merchant', 'Auth/RegisterController@showMerchantForm');
+Route::post('/register/merchant', 'Auth/RegisterController@postMerchantForm');
+
+/** Agent Login/Register */
+Route::get('/login/agent', 'Auth/LoginController@showAgentForm');
+Route::post('/login/agent', 'Auth/LoginController@postAgentForm');
+Route::get('/register/agent', 'Auth/RegisterController@showAgentForm');
+Route::post('/register/agent', 'Auth/RegisterController@postAgentForm');
