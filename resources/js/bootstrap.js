a44341ac396bb65a662c21b6ba5fe73383abc50d
manishadhikari241 <manishadhikari241@gmail.com>
2019-11-14 12:31:44 +0545

    ok

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,7 +5,7 @@ require('bootstrap');
 require('admin-lte');
 
 /**
- * We'll load the axios HTTP library which allows us to easily issue requests
+ * We'll load the axios HTTP library which allows us to easily issue requests12
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
